e27aa5449be20074fb8e1a518179472b5fbca8de
nownabe <nownabe@gmail.com>
2020-12-25 17:00:23 +0900

    Analyze ROM

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        error!("ROM filename is required");
+        return Err("ROM filename is required".into());
+    }
     let rom_filename = &args[1];
 
     debug!("ROM file = {}", rom_filename);
@@ -17,6 +21,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut f = File::open(rom_filename)?;
     let mut buf = Vec::new();
     let _ = f.read_to_end(&mut buf)?;
-    println!("{:?}", &buf[0..4]);
+
+    let head = &buf[0..4];
+    if head != [0x4e, 0x45, 0x53, 0x1a] {
+        error!("ROM must be iNES format");
+        return Err("ROM must be iNES format".into());
+    }
+
+    let program_data_size = &buf[4];
+    debug!("program data size = {}", program_data_size);
+    let character_data_size = &buf[5];
+    debug!("character data size = {}", character_data_size);
+
     Ok(())
 }
